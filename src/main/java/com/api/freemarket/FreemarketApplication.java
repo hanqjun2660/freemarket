9f3e09d1c7e0d441545d72b77778bc30afc0c277
KANG\b2000 <hanqjun2660@gmail.com>
2024-05-08 11:39:18 +0900

    init

---
+++ src/main/java/com/api/freemarket/FreemarketApplication.java
@@ -0,0 +1,13 @@
+package com.api.freemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreemarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/freemarket/FreemarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.freemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreemarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
