1a71410893b2d561dba2b0652d38d1fa268754c2
KANG\b2000 <hanqjun2660@gmail.com>
2024-05-21 17:56:30 +0900

    fix: 전역예외처리 일부 수정

--- src/main/java/com/api/freemarket/common/excption/GlobalExceptionHandler.java
+++ src/main/java/com/api/freemarket/common/excption/GlobalExceptionHandler.java
@@ -2,15 +2,15 @@ package com.api.freemarket.common.excption;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.ErrorResponse;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.io.FileNotFoundException;
 
-import static com.api.freemarket.common.excption.ErrorCode.*;
+import static com.api.freemarket.common.excption.ErrorCode.DATA_NOT_FOUND;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
