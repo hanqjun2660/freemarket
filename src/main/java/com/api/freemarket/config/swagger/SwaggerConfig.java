760d2f0759a3f63f9473da6d44a61afb3faa1196
KANG\b2000 <hanqjun2660@gmail.com>
2024-05-21 14:03:16 +0900

    fix: swagger 설정 테스트

--- src/main/java/com/api/freemarket/config/swagger/SwaggerConfig.java
+++ src/main/java/com/api/freemarket/config/swagger/SwaggerConfig.java
@@ -5,9 +5,18 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SwaggerConfig {
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    }
 
     @Bean
     public OpenAPI openAPI() {
