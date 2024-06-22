46d5bb566d185fc8402ff5aa954b9e027700e990
KANG\b2000 <hanqjun2660@gmail.com>
2024-06-22 18:32:41 +0900

    fix: Swagger Logout api 테스트시 Failed to fetch.Possible Reasons 발생 조치

--- src/main/java/com/api/freemarket/config/swagger/SwaggerConfig.java
+++ src/main/java/com/api/freemarket/config/swagger/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -45,6 +46,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
         return new OpenAPI()
                 .components(components)
+                .addServersItem(new Server().url("/"))
                 .addSecurityItem(securityRequirement)
                 .info(info);
     }
